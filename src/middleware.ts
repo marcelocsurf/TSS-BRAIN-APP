00f3224f7fb33c6056cd672f3257ef8522cbaf3a
marcelocsurf <marcelocsurf@gmail.com>
2026-03-14 19:19:35 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,7 +27,7 @@ export async function middleware(request: NextRequest) {
 
   const { data: { user } } = await supabase.auth.getUser();
 
-  if (request.nextUrl.pathname.startsWith('/portal')) {
+if (request.nextUrl.pathname.startsWith('/portal') || request.nextUrl.pathname.startsWith('/intake')) {
     return supabaseResponse;
   }
 
